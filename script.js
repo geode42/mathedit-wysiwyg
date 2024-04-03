677e24e3101d6dd9f982e4b6a7d1c3aef885d504
geode42 <99060684+geode42@users.noreply.github.com>
2024-04-03 19:50:45 -0400

    move textarea keydown into its own function and add them to containersWithMathFields

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const MQ = MathQuill.getInterface(3)
 const linesContainer = document.getElementById('lines')
 const activeLineIndex = 0
-const containersWithMathFields = []
+const containersWithMathFieldsAndTextAreas = []
 
 function getMathFieldForContainer(container) {
-	return containersWithMathFields.find(i => i.container == container).mathField
+	return containersWithMathFieldsAndTextAreas.find(i => i.container == container).mathField
 }
 
 function insertNewEditorBelow(currentEditorContainer, focus=true) {
@@ -76,91 +76,99 @@ function checkIfCursorIsAtRightOfMathField(container) {
 	return rootBlock.lastElementChild == cursor
 }
 
+function getCustomMathFieldTextareaKeyDown(container, mathField) {
+	const keydownHistory = []
+	
+	return e => {
+		e.key.length == 1 && keydownHistory.push(e.key)
+
+		// ensure there's an empty mathfield at the end (in case this was the "last empty math field", but no longer is)
+		setTimeout(ensureEmptyLastMathField)
+		
+		// make enter create a new editor below
+		if (e.key == 'Enter') {
+			insertNewEditorBelow(container)
+			e.preventDefault()
+		}
+
+		// delete math field if it's empty
+		if (e.key == 'Backspace' && mathField.latex() == '' && container != linesContainer.firstElementChild) {
+			const previousContainer = container.previousElementSibling
+			getMathFieldForContainer(previousContainer).focus()
+			container.remove()
+		}
+
+		// go to the math field above if the cursor isn't in a fraction or something, or to the left if you're already at the top
+		if (e.key == 'ArrowUp' && checkIfCursorIsAtTopOfMathField(container)) {
+			container.previousElementSibling ? getMathFieldForContainer(container.previousElementSibling).focus() : mathField.moveToLeftEnd()
+		}
+		// same but below
+		if (e.key == 'ArrowDown' && checkIfCursorIsAtBottomOfMathField(container)) {
+			container.nextElementSibling ? getMathFieldForContainer(container.nextElementSibling).focus() : mathField.moveToRightEnd()
+		}
+		// same but left
+		if (e.key == 'ArrowLeft' && checkIfCursorIsAtLeftOfMathField(container) && container.previousElementSibling) {
+			getMathFieldForContainer(container.previousElementSibling).focus()
+		}
+		// same but right
+		if (e.key == 'ArrowRight' && checkIfCursorIsAtRightOfMathField(container) && container.nextElementSibling) {
+			getMathFieldForContainer(container.nextElementSibling).focus()
+		}
+
+		const checkKeysToCmdReplacement = (keys, cmd) => {
+			if (e.key.length != 1) return
+			if (e.ctrlKey) return
+			if (keydownHistory.slice(-keys.length).join('') == keys) {
+				mathField.keystroke(Array(keys.length - 1).fill('Backspace').join(' '))
+				e.preventDefault()
+				mathField.cmd(cmd)
+			}
+		}
+
+		checkKeysToCmdReplacement('+-', '\\pm')
+		checkKeysToCmdReplacement('deg', '\\degree')
+		checkKeysToCmdReplacement('!=', '\\ne')
+		checkKeysToCmdReplacement('~=', '\\approx')
+		checkKeysToCmdReplacement('-^', '\\uparrow')
+		checkKeysToCmdReplacement('-v', '\\downarrow')
+		checkKeysToCmdReplacement('<-', '\\leftarrow')
+		checkKeysToCmdReplacement('->', '\\rightarrow')
+		checkKeysToCmdReplacement('=^', '\\Uparrow')
+		checkKeysToCmdReplacement('=v', '\\Downarrow')
+		checkKeysToCmdReplacement('<=', '\\Leftarrow')
+		checkKeysToCmdReplacement('=>', '\\Rightarrow')
+		
+		// make tab create a new editor if you're on the last one
+		// commented-out because tab is also used by mathquill
+		// if (e.key == 'Tab' && !e.shiftKey) {
+		// 	if (linesContainer.lastChild == container) {
+		// 		insertNewEditorBelow(container)
+		// 		e.preventDefault()
+		// 	}
+		// }
+	}
+}
+
 function createEditor() {
 	const container = document.createElement('div')
+	// When I added a keydown listener after I created the mathfield, the mathfield would update and move the cursor and all before my event listener was called.
+	// Apparently most browsers call event listeners in the order they were given, so my event listener had to be added *before* MathQuill's.
+	// hence I give the textarea a placeholder event listener at first, and then after I create the mathfield I replace it with the actual one.
+	let textAreaKeyDownListener = () => {}
+	const textArea = document.createElement('textarea')
+	textArea.addEventListener('keydown', e => {
+		textAreaKeyDownListener(e)
+	})
 	const mathField = MQ.MathField(container, {
 		autoCommands: 'pi theta sqrt sum',
 		autoOperatorNames: 'sin cos tan csc sec cot asin acos atan arcsin arccos arctan',
-		substituteTextarea() {
-			const newTextarea = document.createElement('textarea')
-			const keydownHistory = []
-			
-			newTextarea.onkeydown = e => {
-				e.key.length == 1 && keydownHistory.push(e.key)
-
-				// ensure there's an empty mathfield at the end (in case this was the "last empty math field", but no longer is)
-				setTimeout(ensureEmptyLastMathField)
-				
-				// make enter create a new editor below
-				if (e.key == 'Enter') {
-					insertNewEditorBelow(container)
-					e.preventDefault()
-				}
-
-				// delete math field if it's empty
-				if (e.key == 'Backspace' && mathField.latex() == '' && container != linesContainer.firstElementChild) {
-					const previousContainer = container.previousElementSibling
-					getMathFieldForContainer(previousContainer).focus()
-					container.remove()
-				}
-
-				// go to the math field above if the cursor isn't in a fraction or something, or to the left if you're already at the top
-				if (e.key == 'ArrowUp' && checkIfCursorIsAtTopOfMathField(container)) {
-					container.previousElementSibling ? getMathFieldForContainer(container.previousElementSibling).focus() : mathField.moveToLeftEnd()
-				}
-				// same but below
-				if (e.key == 'ArrowDown' && checkIfCursorIsAtBottomOfMathField(container)) {
-					container.nextElementSibling ? getMathFieldForContainer(container.nextElementSibling).focus() : mathField.moveToRightEnd()
-				}
-				// same but left
-				if (e.key == 'ArrowLeft' && checkIfCursorIsAtLeftOfMathField(container) && container.previousElementSibling) {
-					getMathFieldForContainer(container.previousElementSibling).focus()
-				}
-				// same but right
-				if (e.key == 'ArrowRight' && checkIfCursorIsAtRightOfMathField(container) && container.nextElementSibling) {
-					getMathFieldForContainer(container.nextElementSibling).focus()
-				}
-
-				const checkKeysToCmdReplacement = (keys, cmd) => {
-					if (e.key.length != 1) return
-					if (e.ctrlKey) return
-					if (keydownHistory.slice(-keys.length).join('') == keys) {
-						mathField.keystroke(Array(keys.length - 1).fill('Backspace').join(' '))
-						e.preventDefault()
-						mathField.cmd(cmd)
-					}
-				}
-
-				checkKeysToCmdReplacement('+-', '\\pm')
-				checkKeysToCmdReplacement('deg', '\\degree')
-				checkKeysToCmdReplacement('!=', '\\ne')
-				checkKeysToCmdReplacement('~=', '\\approx')
-				checkKeysToCmdReplacement('-^', '\\uparrow')
-				checkKeysToCmdReplacement('-v', '\\downarrow')
-				checkKeysToCmdReplacement('<-', '\\leftarrow')
-				checkKeysToCmdReplacement('->', '\\rightarrow')
-				checkKeysToCmdReplacement('=^', '\\Uparrow')
-				checkKeysToCmdReplacement('=v', '\\Downarrow')
-				checkKeysToCmdReplacement('<=', '\\Leftarrow')
-				checkKeysToCmdReplacement('=>', '\\Rightarrow')
-			}
-			// make tab create a new editor if you're on the last one
-			// commented-out because tab is also used by mathquill
-			// newTextarea.onkeydown = e => {
-			// 	if (e.key == 'Tab' && !e.shiftKey) {
-			// 		if (linesContainer.lastChild == container) {
-			// 			insertNewEditorBelow(container)
-			// 			e.preventDefault()
-			// 		}
-			// 	}
-			// }
-			return newTextarea
-		}
+		substituteTextarea: () => textArea,
 	})
-	containersWithMathFields.push({ mathField, container })
-	return { mathField, container }
+	textAreaKeyDownListener = getCustomMathFieldTextareaKeyDown(container, mathField)
+	containersWithMathFieldsAndTextAreas.push({ mathField, container, textArea })
+	return { mathField, container, textArea }
 }
 
 // add first editor
 linesContainer.append(createEditor().container)
-containersWithMathFields[0].mathField.focus()
+containersWithMathFieldsAndTextAreas[0].mathField.focus()
